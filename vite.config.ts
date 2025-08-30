0b11b959e9c93feae1485b15ff689d0797442a90
Rasheed Sharaf Aldeen <134731625+Rasheed1506993@users.noreply.github.com>
2025-08-30 17:23:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/bader-drinkshop/",
   server: {
     host: "::",
     port: 8080,
